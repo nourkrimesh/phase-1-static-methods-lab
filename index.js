e69c2c4daebaf9abf15c3d703ee804c2db6b622d
Nour Krimesh <nour.krimesh@gmail.com>
2023-07-12 01:08:15 +0300

    Complete assignment

--- index.js
+++ index.js
@@ -1,3 +1,25 @@
 class Formatter {
   //add static methods here
+
+  static capitalize(str) {
+    return str.charAt(0).toUpperCase() + str.slice(1);
+  }
+
+  static sanitize(str) {
+    return str.replace(/[^A-Za-z0-9-' ]+/g, "");
+  }
+
+  static titleize(sentence) {
+    const lowercaseWords = ['the', 'a', 'an', 'but', 'of', 'and', 'for', 'at', 'by', 'from'];
+
+    const words = sentence.toLowerCase().split(' ');
+    const capitalizedWords = words.map((word, index) => {
+      if (index === 0 || !lowercaseWords.includes(word)) {
+        return word.charAt(0).toUpperCase() + word.slice(1);
+      }
+      return word;
+    });
+
+    return capitalizedWords.join(' ');
+  }
 }
