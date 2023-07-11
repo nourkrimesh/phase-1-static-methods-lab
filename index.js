6627bcc8e382eccfd7657e757c1757682a672a45
Nour Krimesh <nour.krimesh@gmail.com>
2023-07-12 01:10:14 +0300

    Complete assignment

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ class Formatter {
     const words = sentence.toLowerCase().split(' ');
     const capitalizedWords = words.map((word, index) => {
       if (index === 0 || !lowercaseWords.includes(word)) {
-        return word.charAt(0).toUpperCase() + word.slice(1);
+        return this.capitalize(word);
       }
       return word;
     });
